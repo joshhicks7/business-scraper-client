51aea1b9ae86b34d4c8c481fa4fdf176a08e6545
Josh Hicks <jhjoshrhicks07@gmail.com>
2025-12-24 08:09:52 -0600

    updating client

--- src/components/BusinessList.jsx
+++ src/components/BusinessList.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { Filter, Download, Search as SearchIcon } from 'lucide-react';
 import BusinessCard from './BusinessCard';
 import './BusinessList.css';
